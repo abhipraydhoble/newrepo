50f3d52733f524aa2ae3efe1baaeaa0f74be2341
abhipraydhoble <122669982+abhipraydhoble@users.noreply.github.com>
2024-12-17 13:02:02 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,14 +7,7 @@ resource "aws_instance" "example" {
   ami           = "ami-047126e50991d067b"
   instance_type = "t2.micro"
   tags = {
-    Name = "Webserver-2"
+    Name = "DevOps-B41"
   }
 }
 
-resource "aws_instance" "demo" {
-  ami           = "ami-047126e50991d067b"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "Webserver-345"
-  }
-}
